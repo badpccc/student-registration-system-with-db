7f53ca38d5ac044f2b8fe5d3187b7c1a34bccf31
badpccc <estudosjoaoand@gmail.com>
2026-09-19 16:08:24 -0300

    create aluno service

---
+++ src/main/java/com/example/sistema_alunos/service/AlunoService.java
@@ -0,0 +1,33 @@
+package com.example.sistema_alunos.service;
+
+import com.example.sistema_alunos.model.Aluno;
+import com.example.sistema_alunos.repository.AlunoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    private final AlunoRepository repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Aluno> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Aluno buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Aluno cadastrar(Aluno aluno) {
+        return repository.save(aluno);
+    }
+
+    public void exccluir(Long id) {
+        repository.deleteById(id);
+    }
+}
