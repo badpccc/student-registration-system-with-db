2c3390c1a0b1a44779d8f0115f24a0522c62bd4f
badpccc <estudosjoaoand@gmail.com>
2026-09-19 14:20:48 -0300

    create project structure && initialize Spring Boot

---
+++ src/main/java/com/example/sistema_alunos/SistemaAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Configura o Projeto e Encontra os Componentes.
+public class SistemaAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAlunosApplication.class, args);
+	}
+
+}
